aaf99f8eafe69d8ce5665faa4db923235a8e0f31
lejianwen <84855512@qq.com>
2018-07-27 19:50:17 +0800

    init

---
+++ nginx_install.sh
@@ -0,0 +1,347 @@
+#!/bin/sh
+
+function addnginxservice
+{  
+	
+
+		if [ ! -f "/usr/lib/systemd/system/nginx.service" ]
+		then
+			echo "是否创建nginx.service，交给sysctemd 管理，输入 yes 是"
+			read isyes
+
+			if [ -z "$isyes" ]
+			then
+			echo "not set, keep old set"
+			elif [ $isyes = "yes" ]
+			then		
+				touch /usr/lib/systemd/system/nginx.service
+				true > /usr/lib/systemd/system/nginx.service
+				echo '[Unit]' 												  >> /usr/lib/systemd/system/nginx.service
+				echo 'Description=The Nginx Manager'                          >> /usr/lib/systemd/system/nginx.service
+				echo 'After=network.target'                                   >> /usr/lib/systemd/system/nginx.service
+	            echo ''                                                       >> /usr/lib/systemd/system/nginx.service
+				echo '[Service]'                                              >> /usr/lib/systemd/system/nginx.service
+				echo 'Type=forking'                                           >> /usr/lib/systemd/system/nginx.service
+				echo "PIDFile=$setuppath/$nginxname/logs/nginx.pid"           >> /usr/lib/systemd/system/nginx.service
+				echo "ExecStart=$setuppath/$nginxname/sbin/nginx"             >> /usr/lib/systemd/system/nginx.service
+				echo "ExecReload=$setuppath/$nginxname/sbin/nginx -s reload"  >> /usr/lib/systemd/system/nginx.service
+				echo "ExecStop=$setuppath/$nginxname/sbin/nginx -s stop"      >> /usr/lib/systemd/system/nginx.service
+				echo 'Restart=always'                                         >> /usr/lib/systemd/system/nginx.service
+	            echo ''                                                       >> /usr/lib/systemd/system/nginx.service                                     
+				echo '[Install]'                                              >> /usr/lib/systemd/system/nginx.service
+				echo 'WantedBy=multi-user.target'                             >> /usr/lib/systemd/system/nginx.service
+
+			else
+				echo "nginx service is have!"
+			fi
+		fi   
+	
+}
+
+function mkDir #dir
+{
+	if [ ! -d $1 ]
+		then
+		mkdir $1 -p
+	fi
+}
+
+cd /
+if [ ! -d "data" ]
+then
+	mkdir data
+fi
+
+cd /data
+
+if [ ! -d "apps" ]
+then
+	mkdir apps
+fi
+
+if [ ! -d "src" ]
+then
+	mkdir src
+fi
+
+yum -y install gcc gcc-c++
+yum -y install automake autoconf libtool make
+yum -y install zlib zlib-devel openssl openssl-devel pcre pcre-devel
+
+setuppath=/data/apps
+xiazaipath=/data/src
+cd $xiazaipath
+
+
+echo "请输入nginx名称。"
+read nginxname
+#提示用户输入要安装的nginx名称
+cd $setuppath
+if [ ! -d "$nginxname" ]
+then
+
+	cd $xiazaipath
+
+	ifgroup=`cat /etc/group|grep www|wc -l`
+	if [ $ifgroup -eq 0 ]
+	then
+	   groupadd www
+	fi
+	ifuser=`cat /etc/passwd|grep www|wc -l`
+	if [ $ifuser -eq 0 ]
+	then
+	   useradd -g www www -s /sbin/nologin
+	fi
+
+	cd $xiazaipath
+
+	if [ ! -f "nginx.tar.gz" ]
+	then
+	   echo -e "请输入nginx安装包url,比如：\033[32m http://nginx.org/download/nginx-1.15.2.tar.gz \033[0m"
+	   read url
+	   wget $url -O nginx.tar.gz
+	# else
+	#    rm -rf nginx
+	fi
+
+	# luaisyes=''
+
+	echo -e "是否安装ngx_lua,输入 \033[32m yes \033[0m 安装 \n"
+	# echo "一个基于ngx_lua的web应用的高性能轻量级防火墙"
+	read luaisyes
+	if [ -z "$luaisyes" ]
+	then
+		echo "保留旧配置"
+	elif [ $luaisyes = "yes" ]
+	then
+		# luaisyes='yes'
+	    if [ ! -f "LuaJIT.tar.gz" ]
+	    then
+		    echo -e "请输入LuaJIT.tar.gz安装包url,比如：\033[32m http://luajit.org/download/LuaJIT-2.0.5.tar.gz \033[0m"
+			read url;
+			wget $url -O LuaJIT.tar.gz
+		# else
+		#     rm -rf LuaJIT
+	    fi
+		
+		if [ ! -f "lua-nginx-module.tar.gz" ]
+	    then 
+		    echo -e "请输入lua-nginx-module.tar.gz安装包url,比如：\033[32m https://github.com/openresty/lua-nginx-module/archive/v0.10.13.tar.gz \033[0m"
+			read url;
+			wget $url -O lua-nginx-module.tar.gz
+		# else
+		#     rm -rf lua-nginx-module
+	    fi
+		
+		if [ ! -f "ngx_devel_kit.tar.gz" ]
+	    then
+		    echo -e "请输入ngx_devel_kit.tar.gz安装包url,比如：\033[32m https://github.com/simplresty/ngx_devel_kit/archive/v0.3.1rc1.tar.gz \033[0m"
+			read url;
+			wget $url -O ngx_devel_kit.tar.gz
+		# else
+		#     rm -rf ngx_devel_kit
+	    fi
+		
+		mkDir LuaJIT
+		tar -zxvf LuaJIT.tar.gz -C ./LuaJIT --strip-components 1
+		cd LuaJIT
+		make && make install
+		cd ..
+		
+		mkDir lua-nginx-module
+		mkDir ngx_devel_kit
+		tar -zxvf lua-nginx-module.tar.gz  -C ./lua-nginx-module --strip-components 1
+		tar -zxvf ngx_devel_kit.tar.gz  -C ./ngx_devel_kit --strip-components 1
+		
+	    
+		if [ ! -f /etc/profile.bak ]
+		then
+		    cp -a /etc/profile /etc/profile.bak
+		fi
+		
+		export LUAJIT_LIB=/usr/local/lib
+		export LUAJIT_INC=/usr/local/include/luajit-2.0
+
+		
+	else
+	    echo "not set, keep old set"
+	fi
+
+	mkDir nginx
+	tar -zxvf nginx.tar.gz -C ./nginx --strip-components 1
+	cd nginx   
+	
+    if [ $luaisyes = 'yes' ] 
+	then
+		echo "请输入ngx_lua安装模式：1 直接载入， 2 动态载入； 默认 1 "
+		read lua_add_type
+		
+		if [ $lua_add_type = 2 ] 
+		then
+		./configure --user=www \
+		--group=www --prefix=$setuppath/$nginxname \
+		--with-http_stub_status_module \
+		--with-http_ssl_module \
+		--with-http_flv_module \
+		--with-http_gzip_static_module \
+		--with-http_realip_module \
+		--with-http_v2_module \
+		--add-dynamic-module=../lua-nginx-module \
+		--add-dynamic-module=../ngx_devel_kit  \
+		--with-ld-opt="-Wl,-rpath,$LUAJIT_LIB"
+		else
+		./configure --user=www \
+		--group=www --prefix=$setuppath/$nginxname \
+		--with-http_stub_status_module \
+		--with-http_ssl_module \
+		--with-http_flv_module \
+		--with-http_gzip_static_module \
+		--with-http_realip_module \
+		--with-http_v2_module \
+		--add-module=../lua-nginx-module \
+		--add-module=../ngx_devel_kit  \
+		--with-ld-opt="-Wl,-rpath,$LUAJIT_LIB"
+		fi
+	else
+		./configure --user=www \
+		--group=www --prefix=$setuppath/$nginxname \
+		--with-http_stub_status_module \
+		--with-http_ssl_module \
+		--with-http_flv_module \
+		--with-http_gzip_static_module \
+		--with-http_realip_module \
+		--with-http_v2_module     
+	fi
+
+
+	make -j2  && make install
+
+#cd $setuppath
+#chown -R www.www $setuppath/$nginxname
+#chmod -R +x $nginxname
+fi
+
+#修改配置文件
+#修改配置文件
+
+cd $setuppath/$nginxname/conf
+if [ ! -f "nginx.conf.bak" ]
+then
+   mv $setuppath/$nginxname/conf/nginx.conf $setuppath/$nginxname/conf/nginx.conf.bak
+fi
+
+
+touch $setuppath/$nginxname/conf/php.conf
+true > $setuppath/$nginxname/conf/php.conf
+
+echo '        location ~ \.php$ {' >> $setuppath/$nginxname/conf/php.conf
+echo '             try_files $uri =404;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_connect_timeout      180;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_read_timeout         600;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_send_timeout         600;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_buffer_size          4k;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_buffers 8 4k;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_busy_buffers_size 8k;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_temp_file_write_size 8k;' >> $setuppath/$nginxname/conf/php.conf
+#echo '             fastcgi_cache TEST;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_cache_valid 200 302 1h;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_cache_valid 301 1d;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_cache_valid any 1m;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_cache_min_uses 1;' >> $setuppath/$nginxname/conf/php.conf
+#echo '             fastcgi_cache_use_stale error timeout invalid_header http_500;' >> $setuppath/$nginxname/conf/php.conf
+#echo "             fastcgi_cache_path $setuppath/$nginxname/fastcgi_cache levels=1:2 keys_zone=TEST:10m inactive=5m;" >> $setuppath/$nginxname/conf/php.conf         
+echo '             fastcgi_pass   127.0.0.1:9000;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_index  index.php;' >> $setuppath/$nginxname/conf/php.conf
+echo '             fastcgi_param  SCRIPT_FILENAME  $document_root/$fastcgi_script_name;' >> $setuppath/$nginxname/conf/php.conf
+echo '             include        fastcgi_params;' >> $setuppath/$nginxname/conf/php.conf
+echo '        }' >> $setuppath/$nginxname/conf/php.conf
+
+
+
+touch $setuppath/$nginxname/conf/nginx.conf
+true > $setuppath/$nginxname/conf/nginx.conf
+echo 'user  www  www;' >> $setuppath/$nginxname/conf/nginx.conf
+echo 'worker_processes  2;' >> $setuppath/$nginxname/conf/nginx.conf
+
+echo 'error_log  logs/error.log  notice;' >> $setuppath/$nginxname/conf/nginx.conf
+echo 'pid        logs/nginx.pid;' >> $setuppath/$nginxname/conf/nginx.conf
+echo 'events {' >> $setuppath/$nginxname/conf/nginx.conf
+echo '     use epoll;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '     worker_connections  65535;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '}' >> $setuppath/$nginxname/conf/nginx.conf
+
+echo 'http {' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   include       mime.types;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   default_type  application/octet-stream;' >> $setuppath/$nginxname/conf/nginx.conf
+echo "   log_format  main  '\$remote_addr - \$remote_user [\$time_local] \"\$request\"'  " >> $setuppath/$nginxname/conf/nginx.conf
+echo "                     '\$status \$body_bytes_sent \"\$http_referer\" ' " >> $setuppath/$nginxname/conf/nginx.conf
+echo "                     '\"\$http_user_agent\" \"\$http_x_forwarded_for\"';" >> $setuppath/$nginxname/conf/nginx.conf
+echo '   sendfile        on;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   server_names_hash_bucket_size 128;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   client_header_buffer_size 32k;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   large_client_header_buffers 4 32k;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   client_max_body_size 300m;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   tcp_nopush on;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   keepalive_timeout 60;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   tcp_nodelay on;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   server_tokens off;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   client_body_buffer_size 512k;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   proxy_connect_timeout 60;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   proxy_send_timeout 60;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   proxy_read_timeout 60;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   proxy_buffer_size 16k;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   proxy_buffers 4 64k;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   proxy_busy_buffers_size 128k;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   proxy_temp_file_write_size 128k;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   open_file_cache max=204800 inactive=20s;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   open_file_cache_min_uses 1;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   open_file_cache_valid 30s;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   gzip  on;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   gzip_min_length 1k;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   gzip_buffers 4 16k;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   gzip_http_version 1.1;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   gzip_comp_level 2;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   gzip_types text/plain application/x-javascript text/css application/xml;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   gzip_vary on;' >> $setuppath/$nginxname/conf/nginx.conf
+
+echo '   server {' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        listen       80;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        server_name  localhost;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        access_log  logs/host.access.log  main;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        root   /Data/htdocs/happicloud.happigo.com;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        index  index.html index.htm index.php;' >> $setuppath/$nginxname/conf/nginx.conf
+
+echo '        location /nginx-status {' >> $setuppath/$nginxname/conf/nginx.conf
+echo '              stub_status on;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '              access_log off;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        }' >> $setuppath/$nginxname/conf/nginx.conf
+
+echo '        error_page   500 502 503 504  /50x.html;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        location = /50x.html {' >> $setuppath/$nginxname/conf/nginx.conf
+echo '            root   html;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        }' >> $setuppath/$nginxname/conf/nginx.conf
+
+echo '        include php.conf;' >> $setuppath/$nginxname/conf/nginx.conf
+
+
+echo '        location ~ .*\.(gif|jpg|jpeg|png|bmp|zip|exe|txt|ico|rar|htm|html)$' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        {' >> $setuppath/$nginxname/conf/nginx.conf
+echo '             expires 30d;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        }' >> $setuppath/$nginxname/conf/nginx.conf
+
+echo '        location ~ .*\.(swf|mp3|wmv|wma|mp4|mpg|flv)$' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        {' >> $setuppath/$nginxname/conf/nginx.conf
+echo '             expires 30d;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        }' >> $setuppath/$nginxname/conf/nginx.conf
+
+echo '        location ~ .*\.(js|css)?$' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        {' >> $setuppath/$nginxname/conf/nginx.conf
+echo '             expires 30h;' >> $setuppath/$nginxname/conf/nginx.conf
+echo '        }' >> $setuppath/$nginxname/conf/nginx.conf
+
+echo '   }' >> $setuppath/$nginxname/conf/nginx.conf
+echo '   include vhost/*.conf;'  >> $setuppath/$nginxname/conf/nginx.conf
+echo '}' >> $setuppath/$nginxname/conf/nginx.conf
+
+
+addnginxservice
---
+++ php_install.sh
@@ -0,0 +1,197 @@
+#!/bin/sh
+function phpsetup #setuppath #xiazaipath
+{
+	if [ ! -d $1 ]
+	then
+		mkdir $1 -p
+	fi
+	if [ ! -d $2 ]
+	then
+		mkdir $2 -p
+	fi
+	cd $1
+	if [ ! -d "php" ]
+	then
+		cd $2
+		if [ ! -f "php.tar.gz" ]
+		then
+			echo "please input php url,example:http://cn2.php.net/get/php-7.0.28.tar.gz/from/this/mirror"
+			read url
+			wget $url -O php.tar.gz
+		else
+			echo "" #rm -rf php.tar.gz
+		fi
+		if [ ! -d "php" ]
+		then
+			mkdir php
+		fi
+		tar -zxvf php.tar.gz -C ./php --strip-components 1
+		cd php
+		./configure --prefix=$1/php \
+		--with-config-file-path=$1/php/etc \
+		--with-mysqli=mysqlnd \
+		--with-pdo-mysql=mysqlnd \
+		--with-mysql-sock=/tmp/mysql.sock \
+		--enable-mysqlnd \
+		--with-gd \
+		--with-iconv \
+		--with-zlib \
+		--enable-bcmath \
+		--enable-shmop \
+		--enable-sysvsem \
+		--enable-inline-optimization \
+		--enable-mbregex \
+		--enable-fpm \
+		--enable-mbstring \
+		--enable-ftp \
+		--enable-gd-native-ttf \
+		--with-openssl \
+		--enable-pcntl \
+		--enable-sockets \
+		--with-xmlrpc \
+		--enable-zip \
+		--enable-soap \
+		--with-gettext \
+		--with-curl \
+		--with-jpeg-dir \
+		 --enable-opcache \
+		--with-freetype-dir
+		make && make install
+		cp -a ./php.ini-production $1/php/etc/php.ini
+		cp -a $1/php/etc/php-fpm.conf.default $1/php/etc/php-fpm.conf
+		cp -a $1/php/etc/php-fpm.d/www.conf.default $1/php/etc/php-fpm.d/www.conf
+		sed -i 's/user = nobody/user = www/' $1/php/etc/php-fpm.d/www.conf
+		sed -i 's/group = nobody/group = www/' $1/php/etc/php-fpm.d/www.conf
+		sed -i 's/;rlimit_files = 1024/rlimit_files = 65535/' $1/php/etc/php-fpm.d/www.conf
+		sed -i 's/pm.max_children = 5/pm.max_children = 200/' $1/php/etc/php-fpm.d/www.conf
+		sed -i 's/;pm.max_requests = 500/pm.max_requests = 65535/' $1/php/etc/php-fpm.d/www.conf
+		sed -i 's/pm.start_servers = 2/pm.start_servers = 100/' $1/php/etc/php-fpm.d/www.conf
+		sed -i 's/pm.min_spare_servers = 1/pm.min_spare_servers = 10/' $1/php/etc/php-fpm.d/www.conf
+		sed -i 's/pm.max_spare_servers = 3/pm.max_spare_servers = 180/' $1/php/etc/php-fpm.d/www.conf
+		sed -i 's#;slowlog = log/$pool.log.slow#slowlog = log/$pool.log.slow#' $1/php/etc/php-fpm.d/www.conf
+		sed -i 's/;request_terminate_timeout = 0/request_terminate_timeout = 5s/' $1/php/etc/php-fpm.d/www.conf
+		
+		cp -a ./sapi/fpm/php-fpm.service /usr/lib/systemd/system/php-fpm.service
+		systemctl enable php-fpm #开机运行服务
+
+	fi
+}
+function phpset
+{
+	#sed -i 's/DirectoryIndex index.html/DirectoryIndex index.html index.php/' $1/apache/conf/httpd.conf
+	#sed -i '378 i\    AddType application/x-httpd-php-source .phps' $1/apache/conf/httpd.conf
+	#sed -i '378 i\    AddType application/x-httpd-php .php .phtml .php3 .inc' $1/apache/conf/httpd.conf
+	#sed -i '/ttt/ a input content' $1/apache/conf/httpd.conf
+	
+	sed -i 's/disable_functions =/disable_functions =passthru,exec,assert,system,chroot,chgrp,chown,shell_exec,proc_open,proc_get_status,ini_alter,ini_restore,dl,ini_restore,dl,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server,fsocket,popen,pfsockopen,putenv,phpinfo/' $1/php/etc/php.ini
+	sed -i 's/max_execution_time = 30/max_execution_time = 50/' $1/php/etc/php.ini
+	sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 300M/' $1/php/etc/php.ini
+	sed -i 's#;error_log = php_errors.log#error_log = /data/logs/php/php_errors.log#' $1/php/etc/php.ini
+	sed -i 's/expose_php = On/expose_php = off/' $1/php/etc/php.ini
+	
+}
+function setphpredis
+{
+    isyou=`cat $setuppath/php/etc/php.ini|grep "redis.so"|wc -l`
+	if [ $isyou -eq 0 ]
+	then
+		cd $xiazaipath
+		if [ ! -f "phpredis.tgz" ]
+		then
+			echo "please input php url,example:http://pecl.php.net/get/redis-4.0.0.tgz"
+			read url
+			wget $url -O phpredis.tgz
+		else
+			echo "" #rm -rf phpredis.tgz
+		fi
+		if [ ! -d "phpredis" ]
+		then
+			mkdir phpredis
+		fi
+		tar -zxvf phpredis.tgz -C ./phpredis --strip-components 1
+		cd phpredis
+		$setuppath/php/bin/phpize
+		./configure --with-php-config=$setuppath/php/bin/php-config
+		make && make install
+		isyou1=`cat $setuppath/php/etc/php.ini|grep "no-debug-zts-20151012"|wc -l`
+		if [ $isyou1 -eq 0 ]
+		then
+			echo "extension_dir=\"$setuppath/php/lib/php/extensions/no-debug-non-zts-20151012/\"" >> $setuppath/php/etc/php.ini
+		fi
+		echo 'extension = "redis.so"' >> $setuppath/php/etc/php.ini
+	fi
+}
+function setphpswoole
+{
+
+    isyou=`cat $setuppath/php/etc/php.ini|grep "swoole.so"|wc -l`
+	if [ $isyou -eq 0 ]
+	then
+		cd $xiazaipath
+		if [ ! -f "phpswoole.tar.gz" ]
+		then
+			echo "please input php url,example:https://github.com/swoole/swoole-src/archive/v1.10.2.tar.gz"
+			read url
+			wget $url -O phpswoole.tar.gz
+		else
+			echo "" #rm -rf phpswoole.tar.gz
+		fi
+		if [ ! -d "phpswoole" ]
+		then
+			mkdir phpswoole
+		fi
+		tar -zxvf  phpswoole.tar.gz -C ./phpswoole --strip-components 1
+
+		cd phpswoole
+		$setuppath/php/bin/phpize
+		./configure --with-php-config=$setuppath/php/bin/php-config \
+		--enable-openssl
+		make && make install
+		isyou1=`cat $setuppath/php/etc/php.ini|grep "no-debug-zts-20151012"|wc -l`
+		if [ $isyou1 -eq 0 ]
+		then
+			echo "extension_dir=\"$setuppath/php/lib/php/extensions/no-debug-non-zts-20151012/\"" >> $setuppath/php/etc/php.ini
+		fi
+		echo 'extension = "swoole.so"' >> $setuppath/php/etc/php.ini
+	fi
+}
+setuppath=/data/apps
+xiazaipath=/data/src
+yum -y install bison
+yum -y install libXpm-devel
+yum -y install lrzsz curl curl-devel ftp bind-utils nslookup tcpdump man traceroute telnet openssh* sos-2.2-38.el6 wget gc* make openssl openssl-devel pcre-devel zlib-devel zip unzip ncurses-devel ncurses zutoconf libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel e2fsprogs e2fsprogs-devel krb5-devel libidn libidn-devel libxslt-devel libevent libevent-devel libtool-ltdl libtool ntp vim-enhanced python wget lsof iptraf strace dos2unix
+yum -y install libmcrypt libmcrypt-devel mcrypt mhash libcurl-devel
+phpsetup $setuppath $xiazaipath
+phpset $setuppath
+###
+:<<eof
+echo "are you install phpmemcache? input yes is setup,input any is nothing to do"
+read isyes
+if [ -z "$isyes" ]
+then
+	echo "not set, keep old set"
+elif [ $isyes = "yes" ]
+then
+	setphpmemcache
+fi
+eof
+###
+echo "are you install setphpredis? input yes is setup,input any is nothing to do"
+read isyes
+if [ -z "$isyes" ]
+then
+	echo "not set, keep old set"
+elif [ $isyes = "yes" ]
+then
+	setphpredis
+fi
+
+echo "are you install setphpswoole? input yes is setup,input any is nothing to do"
+read isyes
+if [ -z "$isyes" ]
+then
+	echo "not set, keep old set"
+elif [ $isyes = "yes" ]
+then
+	setphpswoole
+fi
