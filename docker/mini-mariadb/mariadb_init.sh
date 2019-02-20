4b91e8cb2ba3bf530f56dcd9a19a31803b6ee52c
lejianwen <84855512@qq.com>
2019-02-20 15:56:18 +0800

    add mini-mariadb

---
+++ docker/mini-mariadb/mariadb_init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# mariadb 经过Dockerfile建立后运行的脚本
+# 主要是因为第一次启动容器可能/data/mysql_data中没有数据，就是mariadb没有初始化，需要先install
+# 或者不使用该sh，将命令写在mariadb-compose.yml中
+
+if [ ! -d /data/mysql_data/mysql ]
+then
+  /data/apps/mysql/scripts/mysql_install_db --basedir=/data/apps/mysql --datadir=/data/mysql_data --user=mysql
+  mysqld start
+  # 添加用户
+  /data/apps/mysql/bin/mysql -e  "grant all on *.* to root@'%' identified by '${MYSQL_ROOT_PASSWORD}'; flush privileges";
+  mysqld stop
+fi
+/data/apps/mysql/bin/mysqld_safe --datadir=/data/mysql_data
