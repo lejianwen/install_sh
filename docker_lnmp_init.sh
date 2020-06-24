17ad14bfbb225dcdd0d54be7ef6642a8e817cf2c
ljw <84855512@qq.com>
2020-06-24 11:12:03 +0800

    fix

--- docker_lnmp_init.sh
+++ docker_lnmp_init.sh
@@ -5,7 +5,7 @@ mkdir -p /data/logs
 mkdir -p /data/docker
 
 cp -a ./data/etc/* /data/etc/
-cp -a ./data/docker/mini-nmpr-compose.yml /data/docker/docker-compose.yml
-cp -a ./data/docker/.env.example /data/docker/.env
+cp -a ./docker/mini-nmpr-compose.yml /data/docker/docker-compose.yml
+cp -a ./docker/.env.example /data/docker/.env
 
 cd /data/docker && docker-compose up -d
